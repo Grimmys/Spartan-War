011a9e268f0f41ccb43e53f63c2109fb6647eba4
Grimmys <funkyzermmy@gmail.com>
2019-07-29 22:54:16 +0200

    Added possibility to move an unit

--- main.lua
+++ main.lua
@@ -6,9 +6,11 @@ MAP_HEIGHT = TILE_SIZE * 50
 SCALING = 0.5
 EFF_TILE_SIZE = TILE_SIZE * SCALING
 TILES_LIMIT_W = 35
-TILES_LIMIT_H = 25
-WINDOW_WIDTH = EFF_TILE_SIZE * TILES_LIMIT_W
-WINDOW_HEIGHT = EFF_TILE_SIZE * TILES_LIMIT_H
+TILES_LIMIT_H = 20
+BORDER_SIZE_W = 50
+BORDER_SIZE_H = 50
+WINDOW_WIDTH = EFF_TILE_SIZE * TILES_LIMIT_W + BORDER_SIZE_W * 2
+WINDOW_HEIGHT = EFF_TILE_SIZE * TILES_LIMIT_H + BORDER_SIZE_H * 2
 
 function love.load()
   --Window configuration
@@ -17,16 +19,23 @@ function love.load()
   end
   
   --Loading images
-  possible_move = love.graphics.newImage("graphics/grid_indicators/move.png")
+    -- Indicators
+    possible_move = love.graphics.newImage("graphics/grid_indicators/move.png")
+    
+    -- Units
+    square = love.graphics.newImage("graphics/units/square.png")
   
   --Map creation
-  map = Map:new{pos = {x = 0, y = 0}, size = {width = MAP_WIDTH, height = MAP_HEIGHT}, sprite = "graphics/maps/first_map.png"}
+  map_sprite = love.graphics.newImage("graphics/maps/first_map.png")
+  map = Map:new{pos = {x = BORDER_SIZE_W, y = BORDER_SIZE_H}, size = {width = MAP_WIDTH, height = MAP_HEIGHT}, sprite = map_sprite}
+  --Part of the map visible at screen
+  map.screen_part = love.graphics.newQuad(0, 0, TILE_SIZE * TILES_LIMIT_W, TILE_SIZE * TILES_LIMIT_H, MAP_WIDTH, MAP_HEIGHT)
   
   --First units (for testing) creation
   UNITS_POS = {{x = 0, y = 0}, {x = EFF_TILE_SIZE * 3, y = EFF_TILE_SIZE * 2}, {x = EFF_TILE_SIZE * 6, y = EFF_TILE_SIZE * 5}}
   units = {}
-  for i = 1, 4 do
-    unit = Unit:new{pos = UNITS_POS[i], size = {width = EFF_TILE_SIZE, height = EFF_TILE_SIZE}, sprite = "graphics/units/square.png", move = 3}
+  for i = 1, 3 do
+    unit = Unit:new{pos = UNITS_POS[i], size = {width = EFF_TILE_SIZE, height = EFF_TILE_SIZE}, sprite = square, relative_to = map, move = 3}
     table.insert(units, unit)
   end
 end
@@ -43,7 +52,7 @@ function love.draw()
   if selected_unit then
     squares = selected_unit:accessibleSquares(map)
     for i, square in ipairs(squares) do
-      love.graphics.draw(possible_move, square.x, square.y, 0, SCALING, SCALING)
+      square:draw()
     end
   end
 end
@@ -53,21 +62,15 @@ end
 function equality(a, b)
   if a == b then
     return true
-  elseif type(a) == "table" and type(b) == "table" then
+  elseif type(a) == "table" and type(b) == "table" and #a == #b then
     for k, v in pairs(a) do
-      if a[k] ~= b[k] then
-        return false
-      end
-    end
-    for k, v in pairs(b) do
-      if a[k] ~= b[k] then
+      if not equality(a[k], b[k]) then
         return false
       end
     end
   else
     return false
   end
-  print("IT'S EQUALS !")
   return true
 end
 
@@ -100,9 +103,13 @@ end
 --CLASSES
 
 --Entity's prototype
-Entity = {pos = {x = 0, y = 0}, size = {width = 0, height = 0}, sprite = ""}
+Entity = {pos = {x = 0, y = 0}, size = {width = 0, height = 0}, sprite = "", relative_to = nil}
 function Entity:draw()
   love.graphics.draw(self.sprite, self.pos.x, self.pos.y, 0, SCALING, SCALING)
+end
+
+function Entity:isTouched(x, y)
+  return x >= self.pos.x and x < self.pos.x + self.size.width and y >= self.pos.y and y < self.pos.y + self.size.height
 end
 
 --Constructor for new entity
@@ -110,24 +117,25 @@ function Entity:new(o)
   ent = o or {}
   setmetatable(ent, self)
   self.__index = self
-  if ent.sprite ~= "" then
-    ent.sprite = love.graphics.newImage(ent.sprite)
+  if ent.relative_to then
+    ent.pos.x = ent.pos.x + ent.relative_to.pos.x
+    ent.pos.y = ent.pos.y + ent.relative_to.pos.y
   end
   return ent
 end
+
+--Square's prototype
+Square = Entity:new()
+Square.size = {width = EFF_TILE_SIZE, height = EFF_TILE_SIZE}
 
 --Unit's prototype
 Unit = Entity:new()
 Unit.move = 0
 
-function Unit:isTouched(x, y)
-  return x >= self.pos.x and x < self.pos.x + self.size.width and y >= self.pos.y and y < self.pos.y + self.size.height
-end
-
 function Unit:accessibleSquares(map)
   --Check if the calcul hasn't be already done
   if not self.available_moves then
-    local squares = {self.pos}
+    local squares = {Square:new{pos = self.pos}}
     
     --Check for available squares, according to unit's move limit
     local prev_squares = squares
@@ -143,35 +151,46 @@ function Unit:accessibleSquares(map)
     --Remove unit's square
     table.remove(squares, 1)
     self.available_moves = squares
-    print("BEGIN")
-    for i, pos in ipairs(self.available_moves) do
-      print(pos.x / EFF_TILE_SIZE, pos.y / EFF_TILE_SIZE)
-    end
-    print("END")
   end
   
   return self.available_moves
 end
 
+function Unit:moveTo(square)
+  --Change the position of the unit
+  self.pos = square.pos
+  
+  --Reset available moves
+  self.available_moves = nil
+end
+
 --Map's prototype
 Map = Entity:new()
+Map.screen_part = {}
 Map.obstacles = {}
 Map.units = {}
+function Map:draw()
+  love.graphics.draw(self.sprite, self.screen_part, self.pos.x, self.pos.y, 0, SCALING, SCALING)
+end
 
 --Method to check all available squares next to the one given
-function Map:nextAvailableSquares(pos)
+function Map:nextAvailableSquares(sq)
   local directions = {{x = 1, y = 0}, {x = -1, y = 0}, {x = 0, y = 1}, {x = 0, y = -1}} --The directions of the next squares 
 
   local squares = {} --Array for available squares
   for i, dir in ipairs(directions) do
-    local square = {x = pos.x + dir.x * EFF_TILE_SIZE, y = pos.y + dir.y * EFF_TILE_SIZE} --Square to test
-    local available = true
-    --Checking if there is no obstacle on the square
-    for i, o in ipairs(self.obstacles) do
-      if square.x == o.x and square.y == o.y then
-        available = false
-        break
+    local square = Square:new{pos = {x = sq.pos.x + dir.x * EFF_TILE_SIZE, y = sq.pos.y + dir.y * EFF_TILE_SIZE}, sprite = possible_move} --Square to test
+    local available = false
+    --Checking if square is not beyond map limit
+    if square.pos.x >= map.pos.x and square.pos.x < map.size.width and square.pos.y >= map.pos.y and square.pos.y < map.size.height then
+      --Checking if there is no obstacle on the square
+      for i, o in ipairs(self.obstacles) do
+        if square.pos.x == o.x and square.pos.y == o.y then
+          available = false
+          break
+        end
       end
+      available = true
     end
     if available then
       table.insert(squares, square)
@@ -192,4 +211,17 @@ function love.mousepressed(x, y, button)
         end
       end
    end
+   
+   if button == 2 then
+     if selected_unit then
+      local available_moves = selected_unit:accessibleSquares(map)
+      for i, square in ipairs(available_moves) do
+        if square:isTouched(x, y) then
+          selected_unit:moveTo(square)
+          selected_unit = nil
+          return
+        end
+      end
+     end
+    end
 end
