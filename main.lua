ce22f959c311f94a996be56637ddd24d635cef75
Grimmys <funkyzermmy@gmail.com>
2019-07-29 16:18:38 +0200

    Updated map (which now use tile size 64)

--- main.lua
+++ main.lua
@@ -1,13 +1,22 @@
+TILE_SIZE = 64
+MAP_WIDTH = TILE_SIZE * 50
+MAP_HEIGHT = TILE_SIZE * 50
+
+-- For now, there is only the map displayed on the window, limited to 30 tiles, and rescaled
+SCALING = 0.25
+TILES_LIMIT = 50
+WINDOW_WIDTH = TILE_SIZE * SCALING * TILES_LIMIT
+WINDOW_HEIGHT = TILE_SIZE * SCALING * TILES_LIMIT
+
 function love.load()
-   image = love.graphics.newImage("graphics/maps/first_map.png")
-   imgX = 0
-   imgY = 0
-   love.graphics.setBackgroundColor(255,255,255)
-   width = 45 * 50 * 0.25
-   height = 45 * 50 * 0.25
-   success = love.window.setMode(width, height)
+   map = love.graphics.newImage("graphics/maps/first_map.png") -- Loading of the first map
+   mapX = 0
+   mapY = 0
+    if not love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT) then
+      print("An error occured during setting window")
+    end
 end
 
 function love.draw()
-    love.graphics.draw(image, imgX, imgY, 0, 0.25, 0.25)
+    love.graphics.draw(map, mapX, mapY, 0, SCALING, SCALING) -- Displaying map, according to ratio
 end
